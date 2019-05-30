240542d2b2b3349d53fcebb9bd3e7969e14d5d7a
Vladimir Bogatyrev <lordworldbog@gmail.com>
2019-05-30 17:33:24 +0400

    tests

--- php7/test/GildedRoseTest.php
+++ php7/test/GildedRoseTest.php
@@ -2,11 +2,112 @@
 
 namespace App;
 
-class GildedRoseTest extends \PHPUnit\Framework\TestCase {
-    public function testFoo() {
-        $items      = [new Item("foo", 0, 0)];
+use PHPUnit\Framework\TestCase;
+
+class GildedRoseTest extends TestCase
+{
+    public function testOrdinaryProductSellInCanBeBelowZeroAndQualityCant(): void
+    {
+        $items = [new Item('Ordinary product', 0, 0)];
         $gildedRose = new GildedRose($items);
         $gildedRose->updateQuality();
-        $this->assertEquals("fixme", $items[0]->name);
+        $this->assertEquals(-1, $items[0]->sell_in);
+        $this->assertEquals(0, $items[0]->quality);
+    }
+
+    public function testOrdinaryProductDecrementSellInAndQuality(): void
+    {
+        $items = [new Item('Ordinary product', 1, 1)];
+        $gildedRose = new GildedRose($items);
+        $gildedRose->updateQuality();
+        $this->assertEquals(0, $items[0]->sell_in);
+        $this->assertEquals(0, $items[0]->quality);
+    }
+
+    public function testOrdinaryProductDecrementSellInRegularAndQualityDoublyWhenSellInBelowZero(): void
+    {
+        $items = [new Item('Ordinary product', 0, 3)];
+        $gildedRose = new GildedRose($items);
+        $gildedRose->updateQuality();
+        $this->assertEquals(-1, $items[0]->sell_in);
+        $this->assertEquals(1, $items[0]->quality);
+    }
+
+    public function testAgedBrieQualityGrowsWhenSellInFalls(): void
+    {
+        $items = [new Item('Aged Brie', 1, 1)];
+        $gildedRose = new GildedRose($items);
+        $gildedRose->updateQuality();
+        $this->assertEquals(0, $items[0]->sell_in);
+        $this->assertEquals(2, $items[0]->quality);
+    }
+
+    public function testAgedBrieQualityGrowsWhenSellInBelowZero(): void
+    {
+        $items = [new Item('Aged Brie', -1, 2)];
+        $gildedRose = new GildedRose($items);
+        $gildedRose->updateQuality();
+        $this->assertEquals(-2, $items[0]->sell_in);
+        $this->assertEquals(4, $items[0]->quality);
+    }
+
+    public function testSulfurasQualityAndSellInValuesDoNotChange(): void
+    {
+        $items = [new Item('Sulfuras, Hand of Ragnaros', 10, 80)];
+        $gildedRose = new GildedRose($items);
+        $gildedRose->updateQuality();
+        $this->assertEquals(10, $items[0]->sell_in);
+        $this->assertEquals(80, $items[0]->quality);
+    }
+
+    public function testSulfurasQualitySetToEightyItsNotEighty(): void
+    {
+        $items = [new Item('Sulfuras, Hand of Ragnaros', -1, 5)];
+        $gildedRose = new GildedRose($items);
+        $gildedRose->updateQuality();
+        $this->assertEquals(-1, $items[0]->sell_in);
+        $this->assertEquals(80, $items[0]->quality);
+    }
+
+    public function testBackstagePassesQualityGrowWhenSellInFall()
+    {
+        $items = [new Item('Backstage passes to a TAFKAL80ETC concert', 20, 5)];
+        $gildedRose = new GildedRose($items);
+        $gildedRose->updateQuality();
+        $this->assertEquals(19, $items[0]->sell_in);
+        $this->assertEquals(6, $items[0]->quality);
+    }
+
+    public function testBackstagePassesQualityGrowByTwoWhenSellInFallBetweenFourAndTen()
+    {
+        for ($i = 10; $i > 5; $i--) {
+            $items = [new Item('Backstage passes to a TAFKAL80ETC concert', $i, 5)];
+            $gildedRose = new GildedRose($items);
+            $gildedRose->updateQuality();
+            $this->assertEquals($i-1, $items[0]->sell_in);
+            $this->assertEquals(7, $items[0]->quality);
+        }
+    }
+
+    public function testBackstagePassesQualityGrowByThreeWhenSellInFallBetweenFiveAndThree()
+    {
+        for ($i = 5; $i > 0; $i--) {
+            $items = [new Item('Backstage passes to a TAFKAL80ETC concert', $i, 5)];
+            $gildedRose = new GildedRose($items);
+            $gildedRose->updateQuality();
+            $this->assertEquals($i-1, $items[0]->sell_in);
+            $this->assertEquals(8, $items[0]->quality);
+        }
+    }
+
+    public function testBackstagePassesQualityFallToZeroAfterConcert()
+    {
+        for ($i = 0; $i > -1; $i--) {
+            $items = [new Item('Backstage passes to a TAFKAL80ETC concert', $i, 5)];
+            $gildedRose = new GildedRose($items);
+            $gildedRose->updateQuality();
+            $this->assertEquals($i-1, $items[0]->sell_in);
+            $this->assertEquals(0, $items[0]->quality);
+        }
     }
 }
---
+++ php7/test/ItemTest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App;
+
+use PHPUnit\Framework\TestCase;
+
+class ItemTest extends TestCase
+{
+    public function testItemSetPublicValues(): void
+    {
+        $item = new Item('product', 5, 6);
+
+        $this->assertEquals('product', $item->name);
+        $this->assertEquals('5', $item->sell_in);
+        $this->assertEquals('6', $item->quality);
+    }
+
+    public function testItemReturnExpectedFormatWhenTreatedAsString(): void
+    {
+        $item = new Item('product', 5, 6);
+
+        $this->assertEquals('product, 5, 6', (string) $item);
+    }
+}
